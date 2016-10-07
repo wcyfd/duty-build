b94cbd78c7bf0ba48d62c0619650cff60b580784
aimfd <1101697681@qq.com>
2016-10-07 20:31:55 +0800

    create

---
+++ src/main/java/com/aim/duty/duty_build/DutyBuildApp.java
@@ -0,0 +1,13 @@
+package com.aim.duty.duty_build;
+
+/**
+ * Hello world!
+ *
+ */
+public class DutyBuildApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
